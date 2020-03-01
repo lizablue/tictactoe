b2c63cc8b5e410b0c0ef7f24861e81607efa3242
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-01 15:48:57 -0600

    display results text below the title

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let cells = document.querySelectorAll('.row > div'); // select all the cells
+let results = document.querySelector('#result');
 
 // listen for click
 for (let i = 0; i < cells.length; i++) {
@@ -18,21 +19,21 @@ function cellClicked() {
     // result = document.getElementById('#h3');
 
     if (checkCombo(0, 1, 2)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(3, 4, 5)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(6, 7, 8)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(0, 3, 6)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(1, 4, 7)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(2, 5, 8)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(0, 4, 8)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     } else if (checkCombo(2, 4, 6)) {
-        console.log("WINNER: " + turn);
+        results.textContent = "WINNER: " + turn
     }
     if (result.length > 0) {
         return;
@@ -57,22 +58,6 @@ function checkCombo(a, b, c) {
         return false;
     }
 };
-
-//     function getResult() {
-//         // let draw = $('#box').length
-//         const result = h3.innerHTML;
-//         if (div.contains("X" || "O")) {
-//             h3.innerHTML = "draw";
-//         }
-//         // } else if (cells[0].textContent === "X" && cells[1].textContent === "X" && cells[2].textContent === "X") {
-//         //     console.log("WINNER: X!")
-//         // } else if (cells[0] && cells[1] && cells[2] === "O") {
-//         //     consolg.log("WINNER: O!")
-//         // }
-//     };
-
-
-
 
 // HINTS
 // if else statements are your friend
